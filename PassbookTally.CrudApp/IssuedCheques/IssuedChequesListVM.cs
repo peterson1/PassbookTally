1963e565a7bc0974d128e42eebea6197ce4eb2df
Peterson Salamat <petersonsalamat@gmail.com>
2018-04-29 03:22:33 +0800

    Editable issued cheques

--- PassbookTally.CrudApp/IssuedCheques/IssuedChequesListVM.cs
+++ PassbookTally.CrudApp/IssuedCheques/IssuedChequesListVM.cs
@@ -3,6 +3,7 @@ using CommonTools.Lib45.BaseViewModels;
 using CommonTools.Lib45.InputCommands;
 using CommonTools.Lib45.InputDialogs;
 using CommonTools.Lib45.LiteDbTools;
+using PassbookTally.DatabaseLib.Repositories;
 using PassbookTally.DatabaseLib.StateTransitions;
 using PassbookTally.DomainLib.DTOs;
 using PassbookTally.DomainLib45.Configuration;
@@ -39,11 +40,24 @@ namespace PassbookTally.CrudApp.IssuedCheques
         }
 
 
+        protected override void LoadRecordForEditing(RequestedChequeDTO rec)
+        {
+            if (!PopUpInput.TryGetString("Issued To", out string issuedTo, rec.IssuedTo)) return;
+            if (!PopUpInput.TryGetDate("Date Issued", out DateTime issuedDate, rec.IssuedDate)) return;
+            rec.IssuedTo = issuedTo;
+            rec.IssuedDate = issuedDate;
+            Repo.Update(rec);
+        }
+
+
         protected override List<RequestedChequeDTO> QueryItems(SharedCollectionBase<RequestedChequeDTO> db)
             => db.GetAll().Where(_ => _.IssuedDate.HasValue
                                    && _.Request.BankAccountId == _mainWin.AccountId).ToList();
 
 
         protected override Func<RequestedChequeDTO, decimal> SummedAmount => _ => _.Request.Amount.Value;
+
+
+        private ActiveChequesRepo Repo => _mainWin.AppArgs.DCDR.ActiveCheques;
     }
 }
